d129db8ba061d2464882f25834cd1ba3c5e4d9f2
Nick <nituev7@gmail.com>
2015-02-01 14:04:05 +0300

    Add Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,21 @@
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  concat: {
-    options: {
-      stripBanners: true,
-      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %> */',
-    },
-    dist: {
-      src: ['src/project.js'],
-      dest: 'dist/built.js',
-    },
-  },
-});
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
